e952de897fbdce90bed36ed9aa815ef516a60fff
majun <majun@asiainfo.com>
2016-03-17 17:58:51 +0800

    first commit

---
+++ src/main/java/com/ai/baas/bmc/topoligy/core/flow/GPRSGeneralFlow.java
@@ -0,0 +1,31 @@
+package com.ai.baas.bmc.topoligy.core.flow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ai.baas.bmc.topoligy.core.bolt.DuplicateCheckingBolt;
+import com.ai.baas.bmc.topoligy.core.bolt.UnpackingBolt;
+import com.ai.baas.storm.flow.BaseFlow;
+import com.ai.baas.storm.util.BaseConstants;
+
+/**
+ * GPRS通用拓扑图
+ * @author majun
+ * @since 2016.3.16
+ */
+public class GPRSGeneralFlow extends BaseFlow {
+	private static Logger logger = LoggerFactory.getLogger(GPRSGeneralFlow.class);
+	
+	@Override
+	public void define() {
+		super.setKafkaSpout();
+		builder.setBolt("unpacking", new UnpackingBolt(), 1).shuffleGrouping(BaseConstants.KAFKA_SPOUT_NAME);
+		builder.setBolt("duplicate-checking", new DuplicateCheckingBolt(), 1).shuffleGrouping("unpacking");
+	}
+
+	public static void main(String[] args) {
+		GPRSGeneralFlow flow = new GPRSGeneralFlow();
+		flow.run(args);
+	}
+
+}
---
+++ src/main/java/com/ai/baas/bmc/topoligy/core/util/BmcConstants.java
@@ -0,0 +1,8 @@
+package com.ai.baas.bmc.topoligy.core.util;
+
+public class BmcConstants {
+
+	public static final String FIELD_SPLIT = new String(new char[] { (char) 1 });
+	public static final String RECORD_SPLIT = new String(new char[] { (char) 2 });
+	
+}
